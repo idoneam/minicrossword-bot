2f366f6f2aad6ff2df83c11b92fe869dc716b272
David Lougheed <david.lougheed@gmail.com>
2020-07-24 18:24:40 -0400

    Append to log instead of overwriting

--- Main.py
+++ Main.py
@@ -27,7 +27,7 @@ DEVELOPER_ROLE = "idoneam"
 # Logging configuration
 logger = logging.getLogger('discord')
 logger.setLevel(logging.INFO)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='a')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
