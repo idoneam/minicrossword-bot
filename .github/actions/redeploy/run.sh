b8c37cad63d6f7bd2c7a27558dac1c2caf08443b
namemcguffin@not-a-tower <52169277+namemcguffin@users.noreply.github.com>
2021-05-04 04:50:32 -0400

    cleaned up redeploy action structure

--- .github/actions/redeploy/run.sh
+++ .github/actions/redeploy/run.sh
@@ -1,17 +1,16 @@
 #!/bin/sh -l
 
 # 1: vm key
-# 2: vm address
-# 3: vm session
-# 4: project dir
-# 5: project mutex
-# 6: image name
-# 7: args
+# 2: ssh address
+# 3: project dir
+# 4: project mutex
+# 5: image name
+# 6: args
 
 printf "$1" > key.pem
 chmod 400 key.pem
-ssh -o StrictHostKeyChecking=no -i key.pem "$3@$2" "cd '$4' && setlock '.$5' sh -c '
-docker pull $6 &&\
-docker stop $5 &&\
-docker rm $5 &&\
-docker run -d --name=$5 $7 $6'" 2> /dev/null
+ssh -o StrictHostKeyChecking=no -i key.pem "$2" "cd '$3' && setlock '.$4' sh -c '
+docker pull $5 &&\
+docker stop $4 &&\
+docker rm $4 &&\
+docker run -d --name=$4 $6 $5'" 2> /dev/null
