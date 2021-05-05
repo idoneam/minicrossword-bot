3309dc514979e07f1e9201ab7c69a84fed67ecd0
namemcguffin@not-a-tower <52169277+namemcguffin@users.noreply.github.com>
2021-05-05 19:07:45 -0400

    change: added strict host key checking

--- .github/actions/redeploy/run.sh
+++ .github/actions/redeploy/run.sh
@@ -2,18 +2,30 @@
 
 # 1: vm key
 # 2: ssh address
-# 3: project dir
-# 4: project mutex
-# 5: image name
-# 6: args
+# 3: public key
+# 4: ssh user
+# 5: project dir
+# 6: project mutex
+# 7: image name
+# 8: args
 
-printf "$1" > key.pem
-chmod 400 key.pem
-ssh -o StrictHostKeyChecking=no -i key.pem "$2" "\
-cd '$3' && \
-setlock '.$4' sh -c '
-docker pull $5 2>&1 && \
-docker stop $4 2>&1 && \
-docker rm $4 2>&1 && \
-docker run -d --name=$4 $6 $5 2>&1' && \
-rm '.$4'" 2> /dev/null
+printf "$1" > private.key
+chmod 400 private.key
+if [ -z "$3" ]
+then
+    echo 'not verifying host key'
+    strict='no'
+else
+    echo 'checking host key'
+    strict='yes'
+    printf "$2 $3" > host.pub
+    chmod 644 host.pub
+fi
+ssh -o StrictHostKeyChecking="$strict" -o UserKnownHostsFile=host.pub -i private.key "$4@$2" "\
+cd '$5' && \
+setlock '.$6' sh -c '
+docker pull $7 2>&1 && \
+docker stop $6 2>&1 && \
+docker rm $6 2>&1 && \
+docker run -d --name=$6 $8 $7 2>&1' && \
+rm '.$6'" 2> /dev/null
